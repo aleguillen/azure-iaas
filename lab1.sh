7cfdb904a4425b9a6715a0dfaba21797126fd4de
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2022-06-10 14:11:20 -0500

    Adding Lab 1 code

--- lab1.sh
+++ lab1.sh
@@ -1,4 +1,4 @@
-sh ./setup-variables.sh
+sh ./setup-variables.sh -d algutws -l southcentralus -i 1 -g ""
 
 echo "Current Azure Subscription:"
 az account show
@@ -7,18 +7,56 @@ echo "Creating Resource Group:"
 az group create --location $LOCATION --name $RG_NAME
 
 echo "Creating Virtual Network:"
-az 
+az network vnet create \
+    --resource-group $RG_NAME \
+    --location $LOCATIONC \
+    --name $VNET_NAME \
+    --address-prefixes 10.0.0.0/16 \
+    --subnet-name lb-snet \
+    --subnet-prefixes 10.0.0.0/24
+
+az network vnet subnet create \
+    --name pe-snet \
+    --resource-group $RG_NAME \
+    --vnet-name $VNET_NAME \
+    --address-prefixes 10.0.1.0/24 \
+    --disable-private-link-service-network-policies true \
+    --disable-pro
+
+echo "Creating NSG for Subnets Security"
+az network nsg create \
+    --resource-group $RG_NAME \
+    --name $NSG_NAME
+
+echo "For Demo purposes - Create HTTP Allow rule from internet"
+az network nsg rule create \
+    --resource-group $RG_NAME \
+    --nsg-name $NSG_NAME \
+    --name HTTPAllowRule \
+    --protocol '*' \
+    --direction inbound \
+    --source-address-prefix '*' \
+    --source-port-range '*' \
+    --destination-address-prefix '*' \
+    --destination-port-range 80 \
+    --access allow \
+    --priority 200
 
 if [[ -n "$VM_IMAGE" ]]
 then
-  echo "not Empty"
+  echo "IMAGE FOUND: $GOLDEN_IMAGE"
+  IMAGE=$GOLDEN_IMAGE
+
 else
   echo "NO CUSTOM IMAGE SPECIFIED"
-  az vm create \
-    --resource-group myResourceGroup \
-    --name myVM \
-    --image Debian \
-    --admin-username azureuser \
-    --generate-ssh-keys
+  IMAGE="UbuntuLTS"
 fi 
 
+echo "Creating VMSS - admin user: azureuser (SSH keys generated)"
+az vmss create \
+    --resource-group $RG_NAME \
+    --name $VMSS_NAME \
+    --image $IMAGE \
+    --upgrade-policy-mode automatic \
+    --admin-username azureuser \
+    --generate-ssh-keys
--- setup-variables.sh
+++ setup-variables.sh
@@ -1,9 +1,35 @@
-DEPLOYMENT_NAME="workshop"
+echo "INPUT VARIABLES:"
+while getopts d:l:i:img: flag
+do
+    case "${flag}" in
+        d) 
+            FLAG_NAME=DEPLOYMENT_NAME
+            DEPLOYMENT_NAME=${OPTARG};;
+        l) 
+            FLAG_NAME=LOCATION
+            LOCATION=${OPTARG};;
+        i) 
+            FLAG_NAME=INDEX
+            INDEX=${OPTARG};;
+        g) 
+            FLAG_NAME=GOLDEN_IMAGE
+            GOLDEN_IMAGE=${OPTARG};;
+    esac
+    echo " - "$FLAG_NAME " = " ${OPTARG}
+done
 
-LOCATION="southcentralus"
+echo "GENERATING VARIABLES:"
+RG_NAME="$DEPLOYMENT_NAME-$LOCATION-rg-$INDEX"
+echo " - RG_NAME = " $RG_NAME
 
-VM_IMAGE=""
+VNET_NAME="$DEPLOYMENT_NAME-$LOCATION-vnet-$INDEX"
+echo " - VNET_NAME = " $VNET_NAME
 
-RG_NAME="$DEPLOYMENT_NAME-$LOCATION-rg-01"
+NSG_NAME="$DEPLOYMENT_NAME-$LOCATION-nsg-$INDEX"
+echo " - NSG_NAME = " $NSG_NAME
 
-VMSS_NAME="$DEPLOYMENT_NAME-$LOCATION-vmss-01"
+VM_NAME="$DEPLOYMENT_NAME-$LOCATION-vm-$INDEX"
+echo " - VM_NAME = " $VM_NAME
+
+VMSS_NAME="$DEPLOYMENT_NAME-$LOCATION-vmss-$INDEX"
+echo " - VMSS_NAME = " $VMSS_NAME
