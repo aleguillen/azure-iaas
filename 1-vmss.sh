b27646dd5cb5ae5e508bbff682522b11ea266ee3
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2022-06-14 20:17:50 +0000

    Setting Boot Diagnostics, and running Run-Command to test Cloud Init and Custom Script configuration

--- 1-vmss.sh
+++ 1-vmss.sh
@@ -30,7 +30,7 @@ az vm create \
     --image $IMAGE \
     --admin-username azureuser \
     --assign-identity \
-    --authentication-type all \
+    --authentication-type ssh \
     --nics "$VM_NAME-nic" \
     --generate-ssh-keys
 
@@ -40,6 +40,12 @@ az network nic ip-config address-pool add \
    --nic-name "$VM_NAME-nic" \
    --resource-group $RG_NAME \
    --lb-name "public-lb"
+
+# Enable Boot Diagnostics for VM
+az vm boot-diagnostics enable \
+  --name $VM_NAME \
+  --resource-group $RG_NAME \
+  --storage "https://$STORAGE_ACCOUNT.blob.core.windows.net"
 
 echo "Creating a Private VMSS - admin user: azureuser (SSH keys generated)"
 az vmss create \
@@ -56,14 +62,26 @@ az vmss create \
     --vnet-name $VNET2_NAME \
     --subnet "private-snet" \
     --zones 1 2 3 \
-    --generate-ssh-keys
+    --generate-ssh-keys \
+    --custom-data "cloud-init.sh"
+
+# Enable Boot Diagnostics for VMSS
+az vmss update \
+  --name $VMSS_NAME \
+  --resource-group $RG_NAME \
+  --set virtualMachineProfile.diagnosticsProfile.bootDiagnostics.enabled=True virtualMachineProfile.diagnosticsProfile.bootDiagnostics.storageUri="https://$STORAGE_ACCOUNT.blob.core.windows.net"
 
 az vmss list-instances \
   --resource-group $RG_NAME \
   --name $VMSS_NAME \
-  --output table
+  --output table --query '[].{InstanceId: instanceId, Name: name, ComputerName: osProfile.computerName, AvailabilityZone: zones[0], LatestModelApplied: latestModelApplied, ImageVersion: storageProfile.imageReference.exactVersion}'
 
-echo "Adding CustomScript Extension to configure NGINX."
+INSTANCE_ID=$(az vmss list-instances \
+  --resource-group $RG_NAME \
+  --name $VMSS_NAME \
+  --output tsv --query [0].instanceId)
+
+echo "Adding CustomScript Extension to configure NGINX inside the VM"
 az vm extension set \
   -n CustomScript \
   --publisher Microsoft.Azure.Extensions \
@@ -72,3 +90,17 @@ az vm extension set \
   --resource-group $RG_NAME \
   --settings '{"fileUris":["https://raw.githubusercontent.com/aleguillen/azure-iaas/main/custom-script-extension.sh"],"commandToExecute":"./custom-script-extension.sh"}'
   
+echo "Testing using Run-Command Welcome page in NGINX - From VM"
+az vm run-command invoke \
+  -g $RG_NAME \
+  -n $VM_NAME \
+  --command-id RunShellScript \
+  --scripts "curl localhost"
+
+echo "Testing using Run-Command Welcome page in NGINX - From VMSS"
+az vmss run-command invoke \
+  --resource-group $RG_NAME \
+  --name $VMSS_NAME \
+  --instance-id $INSTANCE_ID \
+  --command-id RunShellScript \
+  --scripts "curl localhost" 
