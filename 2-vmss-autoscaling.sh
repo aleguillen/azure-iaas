80a17cff1b4ff94832cec82fc8723919a0805a18
Alejandra Guillen <4201052+aleguillen@users.noreply.github.com>
2022-06-15 04:12:01 +0000

    Autoscale based on Storage Queue only

--- 2-vmss-autoscaling.sh
+++ 2-vmss-autoscaling.sh
@@ -12,49 +12,34 @@ az monitor autoscale create \
   --resource $VMSS_NAME \
   --resource-type "Microsoft.Compute/virtualMachineScaleSets" \
   --name "$VMSS_NAME-autoscale" \
-  --min-count 2 \
+  --min-count 1 \
   --max-count 3 \
   --count 2
 
 echo "Create Auto-Scaling Rule based on Storage Queue Message Count."
-echo "IF MessageCount > 3 or CPU > 75%, then SCALE OUT by 1."
+echo "IF MessageCount > 3, then SCALE OUT by 1."
 STORAGE_ID=$(az storage account show --name $RG_NAME --name $STORAGE_ACCOUNT --query id -o tsv)
 
 az monitor autoscale rule create \
-  --resource-group $RG_NAME \
-  --autoscale-name "$VMSS_NAME-autoscale" \
-  --condition "Percentage CPU > 75 avg 5m" \
-  --scale out 1 \
-  --cooldown 10 
-
-az monitor autoscale rule create \
   --autoscale-name "$VMSS_NAME-autoscale" \
   --resource-group $RG_NAME \
-  --condition "ApproximateMessageCount > 3 avg 5m" \
+  --condition "ApproximateMessageCount > 3 avg 1m" \
   --scale out 1 \
-  --cooldown 10 \
+  --cooldown 1 \
   --resource "${STORAGE_ID}/services/queue/queues/wsqueue" 
 
-echo "IF MessageCount < 1 or CPU < 25%, then SCALE IN by 1."
-
-az monitor autoscale rule create \
-  --resource-group $RG_NAME \
-  --autoscale-name "$VMSS_NAME-autoscale" \
-  --condition "Percentage CPU < 25 avg 5m" \
-  --scale in 1 \
-  --cooldown 10 
+echo "IF MessageCount < 1, then SCALE IN by 1."
 
 az monitor autoscale rule create \
   --autoscale-name "$VMSS_NAME-autoscale" \
   --resource-group $RG_NAME \
-  --condition "ApproximateMessageCount < 1 avg 5m" \
+  --condition "ApproximateMessageCount < 1 avg 1m" \
   --scale in 1 \
-  --cooldown 10 \
+  --cooldown 1 \
   --resource "${STORAGE_ID}/services/queue/queues/wsqueue" 
 
 
 echo "Adding Message to Queue to test AutoScaling:"
-
 
 array=(1 2 3 4)
 for index in "${array[@]}"
